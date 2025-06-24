066b23d2a6af75505e73d5a73dc47868529b95e0
VasenkovAA <andrej.500@mail.ru>
2025-06-25 00:07:04 +0300

    fix ruff

--- task/models/__init__.py
+++ task/models/__init__.py
@@ -1,6 +1,6 @@
-from task.models.space import Space
-from task.models.category import Category
-from task.models.location import Location
-from task.models.link import Link
-from task.models.file import File
-from task.models.status import Status
+from task.models.space import Space as Space
+from task.models.category import Category as Category
+from task.models.location import Location as Location
+from task.models.link import Link as Link
+from task.models.file import File as File
+from task.models.status import Status as Status
