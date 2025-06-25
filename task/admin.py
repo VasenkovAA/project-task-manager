e22b66140b06422626886da5527534ef088352c4
VasenkovAA <andrej.500@mail.ru>
2025-06-25 11:00:58 +0300

    add admin view and refactoring ruff

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from models import (
+    Category,
+    File,
+    Link,
+    Location,
+    Space,
+    Status,
+    task,
+)
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(File)
+admin.site.register(Link)
+admin.site.register(Location)
+admin.site.register(Space)
+admin.site.register(Status)
+admin.site.register(task)
--- task/models/category.py
+++ task/models/category.py
@@ -1,25 +1,22 @@
 from django.db import models
 from simple_history.models import HistoricalRecords
 
+
 class Category(models.Model):
     category_name = models.CharField(
-        max_length=255,
-        verbose_name="Category Name",
-        help_text="Enter the name of the category",
-        unique=True
+        max_length=255, verbose_name='Category Name', help_text='Enter the name of the category', unique=True
     )
     category_description = models.TextField(
-        verbose_name="Category Description",
-        help_text="Description of the category",
+        verbose_name='Category Description',
+        help_text='Description of the category',
         blank=True,
-        null=True
     )
     category_settings = models.JSONField(
-        verbose_name="Category Settings",
-        help_text="JSON configuration for the category",
+        verbose_name='Category Settings',
+        help_text='JSON configuration for the category',
         default=dict,
         blank=True,
-        null=True
+        null=True,
     )
 
     history = HistoricalRecords()
